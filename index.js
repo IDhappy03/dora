1e33c3a5e25f44eb2586ac63f64b08022d90dd82
idhappy03 <idhappy03@naver.com>
2020-09-03 10:56:21 +0900

    add clear commnad

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('message', (message) => {
     message.reply('https://discordjs.guide/additional-info/changes-in-v12.html#string-concatenation')
   }
   if(message.content == '!!출첵') {
-    message.channel.reply('아직 어케 만드는지 모름')
+    message.channel.send('아직 어케 만드는지 모름')
   }
   
   if(message.content == '!!ㅔㅑㅜㅎ')
