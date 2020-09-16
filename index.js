a80272aeef01beaa58b3473856ecf9a176a86407
idhappy03 <idhappy03@naver.com>
2020-09-16 09:42:29 +0900

    add clear command

--- index.js
+++ index.js
@@ -1570,7 +1570,7 @@ client.on('message', (message) => {
     embed.addField('user',         `${client.users.size.toLocaleString()}`, true);
     embed.addField('server',       `${client.guilds.size.toLocaleString()}`, true);
     embed.addField('ping',          client.ping + 'ms', true);
-    embed.addField('message',                                              )
+    embed.addField('message',      `${client.user.username}`, true);
     //embed.addField('channel',      `${client.channels.size.toLocaleString()}`, true);
     //embed.addField('Discord.js',   `v${Discord.version}`, true);
     //embed.addField('Node',         `${process.version}`, true);
