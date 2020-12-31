29383e1f2c1b2f6a71312ffb9169b815b4ad8f17
idhappy03 <idhappy03@naver.com>
2020-12-31 09:54:04 +0900

    add clear command

--- index.js
+++ index.js
@@ -1593,7 +1593,36 @@ if(message.content == "!!제작자") {
   message.channel.send(`\`\`\`css\n디스코드 : 삐삐야#1950\`\`\``)
 }
 
+//코로나
+if(message.content === `!!코로나`) {
+  const request = require("request")
 
+
+  let url = "https://apiv2.corona-live.com/stats.json"
+  request(url, (error, response, body) => {
+      let overview = JSON.parse(response.body).overview;
+      overview = {
+          total_confirmed_person: overview.confirmed[0], // 총 확진자수
+          yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+  
+          current_confirmed_person: overview.current[0], // 현재 확진자수
+          current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+  
+  
+      let embed = new Discord.MessageEmbed()
+      embed.setTitle('코로나')
+      embed.setURL('https://corona-live.com')
+      embed.setColor('#FF8000')
+      embed.setDescription('증상이 있으실 경우 주변 접촉자에게 알리신 후 인근 보건소를 찾아주시기 바랍니다')
+      embed.addField(`대한민국 총 확진자수`, `${overview.total_confirmed_person}명`, true)
+      embed.addField(`어제 확진자수`, overview.yesterday_confirmed_person + `명`, true)
+      embed.addField(`오늘 확진자수(집계중)`, overview.current_confirmed_person + `명`, true)
+      // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+      message.channel.send(embed)
+  
+  })
+}
 
 
 });
