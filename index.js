43d53770938052ddd54d12fc8341427fb7f498cc
idhappy03 <idhappy03@naver.com>
2021-02-17 13:13:57 +0900

    add clear command

--- index.js
+++ index.js
@@ -1593,39 +1593,8 @@ if(message.content == "!!제작자") {
   message.channel.send(`\`\`\`css\n디스코드 : 삐삐야#1950\`\`\``)
 }
 
-//코로나
-if(message.content === `!!코로나`) {
-  const request = require("request")
-
-
-  let url = "https://apiv2.corona-live.com/stats.json"
-  request(url, (error, response, body) => {
-      let overview = JSON.parse(response.body).overview;
-      overview = {
-          total_confirmed_person: overview.confirmed[0], // 총 확진자수
-          yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
-  
-          current_confirmed_person: overview.current[0], // 현재 확진자수
-          current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
-      }
-  
-  
-      let embed = new Discord.MessageEmbed()
-      embed.setTitle('코로나')
-      embed.setURL('https://corona-live.com')
-      embed.setColor('#FF8000')
-      embed.setDescription('증상이 있으실 경우 주변 접촉자에게 알리신 후 인근 보건소를 찾아주시기 바랍니다')
-      embed.addField(`대한민국 총 확진자수`, `${overview.total_confirmed_person}명`, true)
-      embed.addField(`어제 확진자수`, overview.yesterday_confirmed_person + `명`, true)
-      embed.addField(`오늘 확진자수(집계중)`, overview.current_confirmed_person + `명`, true)
-      // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
-      message.channel.send(embed)
-  
-  })
-}
 //쉐키
-if(message.content === `<@432038330264190977>`)
-  message.reply("태그 조금만 줄여주세요!")
+
 });
 
 function changeCommandStringLength(str, limitLen = 8) {
@@ -1760,6 +1729,284 @@ if(message.content === `!!초대코드`) {
   if(message.content === '!!돈받기') {
     message.channel.send(`||@everyone||\n\`\`농협 356-1430-6858-93 \`\`여기로 매달 \`7,721\` 원을 후원 해주세요.`)
   }
+
+//코로나
+
+  if(message.content === `!!코로나`) {
+    const request = require("request")
+  
+  
+    let url = "https://apiv2.corona-live.com/stats.json"
+    request(url, (error, response, body) => {
+        let overview = JSON.parse(response.body).overview;
+        overview = {
+            total_confirmed_person: overview.confirmed[0], // 총 확진자수
+            yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+    
+            current_confirmed_person: overview.current[0], // 현재 확진자수
+            current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+        }
+    
+    
+        let embed = new Discord.MessageEmbed()
+        embed.setTitle('코로나')
+        embed.setURL('https://corona-live.com')
+        embed.setColor('#FF8000')
+        embed.setDescription('증상이 있으실 경우 주변 접촉자에게 알리신 후 인근 보건소를 찾아주시기 바랍니다')
+        embed.addField(`대한민국 총 확진자수`, `${overview.total_confirmed_person}명`, true)
+        embed.addField(`어제 확진자수`, overview.yesterday_confirmed_person + `명`, true)
+        embed.addField(`오늘 확진자수(집계중)`, overview.current_confirmed_person + `명`, true)
+        // embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+        message.channel.send(embed)
+    
+    })
+  }
+
+if(message.content === "!!코로나 전체") {
+
+  const request = require("request")
+
+
+  let url = "https://apiv2.corona-live.com/stats.json"
+  request(url, (error, response, body) => {
+      let overview = JSON.parse(response.body).overview;
+      overview = {
+          total_confirmed_person: overview.confirmed[0], // 총 확진자수
+          yesterday_confirmed_person: overview.confirmed[1], // 어제 확진자수
+  
+          current_confirmed_person: overview.current[0], // 현재 확진자수
+          current_confirmed_person_diff: overview.current[1], // diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+  
+      let current = JSON.parse(response.body).current;
+      current = {
+          seoul_confirmed_person: current[0].cases[0], // 서울 현재 확진자 수
+          seoul_confirmed_person_diff: current[0].cases[1], // 서울 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          busan_confirmed_person: current[1].cases[0],//부산 현재 확진자 수
+          busan_confirmed_person_diff: current[1].cases[1],//부산 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          incheon_confirmed_person: current[2].cases[0],//인천 현재 확진자 수
+          incheon_confirmed_person_diff: current[2].cases[1],//인천 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          daegu_confirmed_person: current[3].cases[0],//대구 현재 확진자 수
+          daegu_confirmed_person_diff: current[3].cases[1],//대구 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          gwangju_confirmed_person: current[4].cases[0],//광주 현재 확진자 수
+          gwangju_confirmed_person_diff: current[4].cases[1],//광주 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          daejeon_confirmed_person: current[5].cases[0],//대전 현재 확진자 수
+          daejeon_confirmed_person_diff: current[5].cases[1],//대전 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          ulsan_confirmed_person: current[6].cases[0],//울산 현재 확진자 수
+          ulsan_confirmed_person_diff: current[6].cases[1],//울산 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          sejong_confirmed_person: current[7].cases[0],//세종 현재 확진자 수
+          sejong_confirmed_person_diff: current[7].cases[1],//세종 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          gyeonggi_confirmed_person: current[8].cases[0],//경기 현재 확진자 수
+          gyeonggi_confirmed_person_diff: current[8].cases[1],//경기 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+  
+          gangwon_confirmed_person: current[9 ].cases[0],//강원 현재 확진자 수
+          gangwon_confirmed_person_diff: current[9].cases[1],//강원 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          chungbuk_confirmed_person: current[10].cases[0],//충북 현재 확진자 수
+          chungbuk_confirmed_person_diff: current[10].cases[1],//충북 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          chungnam_confirmed_person: current[11].cases[0],//충남 현재 확진자 수
+          chungnam_confirmed_person_diff: current[11].cases[1],//충남 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          gyeongbuk_confirmed_person: current[12].cases[0],//경북 현재 확진자 수
+          gyeongbuk_confirmed_person_diff: current[12].cases[1],//경북 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          gyeongnam_confirmed_person: current[13].cases[0],//경남 현재 확진자 수
+          gyeongnam_confirmed_person_diff: current[13].cases[1],//경남 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          jeonbuk_confirmed_person: current[14].cases[0],//전북 현재 확진자 수
+          jeonbuk_confirmed_person_diff: current[14].cases[1],//전북 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          jeonnam_confirmed_person: current[15].cases[0],//전남 현재 확진자 수
+          jeonnam_confirmed_person_diff: current[15].cases[1],//전남 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+          
+          jeju_confirmed_person: current[16].cases[0],//제주 현재 확진자 수
+          jeju_confirmed_person_diff: current[16].cases[1],//제주 diff(어제 이 시간대 확진자 수 - 현재 이 시간대 확진자 수)
+      }
+  
+      let overall = JSON.parse(response.body).overall;
+      overall = {
+          seoul_total_confirmed_person: overall[0].cases[0], // 서울 총 확진자수
+          seoul_yesterday_confirmed_person: overall[0].cases[1], // 서울 어제 확진자수
+  
+          busan_total_confirmed_person: overall[1].cases[0], // 부산 총 확진자수
+          busan_yesterday_confirmed_person: overall[1].cases[1], // 부산 어제 확진자수
+  
+          incheon_total_confirmed_person: overall[2].cases[0], // 인천 총 확진자수
+          incheon_yesterday_confirmed_person: overall[2].cases[1], // 인천 어제 확진자수
+  
+          daegu_total_confirmed_person: overall[3].cases[0], // 대구 총 확진자수
+          daegu_yesterday_confirmed_person: overall[3].cases[1], // 대구 어제 확진자수
+          gwangju_total_confirmed_person: overall[4].cases[0], // 광주 총 확진자수
+          gwangju_yesterday_confirmed_person: overall[4].cases[1], // 광주 어제 확진자수
+  
+          daejeon_total_confirmed_person: overall[5].cases[0], // 대전 총 확진자수
+          daejeon_yesterday_confirmed_person: overall[5].cases[1], // 대전 어제 확진자수
+  
+          ulsan_total_confirmed_person: overall[6].cases[0], // 울산 총 확진자수
+          ulsan_yesterday_confirmed_person: overall[6].cases[1], // 울산 어제 확진자수
+  
+          sejong_total_confirmed_person: overall[7].cases[0], // 세종 총 확진자수
+          sejong_yesterday_confirmed_person: overall[7].cases[1], // 세종 어제 확진자수
+  
+          gyeonggi_total_confirmed_person: overall[8].cases[0], // 경기 총 확진자수
+          gyeonggi_yesterday_confirmed_person: overall[8].cases[1], // 경기 어제 확진자수
+          
+          gangwon_total_confirmed_person: overall[9].cases[0], // 강원 총 확진자수
+          gangwon_yesterday_confirmed_person: overall[9].cases[1], // 강원 어제 확진자수
+          
+          chungbuk_total_confirmed_person: overall[10].cases[0], // 충북 총 확진자수
+          chungbuk_yesterday_confirmed_person: overall[10].cases[1], // 충북 어제 확진자수
+          
+          chungnam_total_confirmed_person: overall[11].cases[0], // 충남 총 확진자수
+          chungnam_yesterday_confirmed_person: overall[11].cases[1], // 충남 어제 확진자수
+          
+          gyeongbuk_total_confirmed_person: overall[12].cases[0], // 경북 총 확진자수
+          gyeongbuk_yesterday_confirmed_person: overall[12].cases[1], // 경북 어제 확진자수
+          
+          gyeongnam_total_confirmed_person: overall[13].cases[0], // 경남 총 확진자수
+          gyeongnam_yesterday_confirmed_person: overall[13].cases[1], // 경남 어제 확진자수
+          
+          jeonbuk_total_confirmed_person: overall[14].cases[0], // 전북 총 확진자수
+          jeonbuk_yesterday_confirmed_person: overall[14].cases[1], // 전북 어제 확진자수
+          
+          jeonnam_total_confirmed_person: overall[15].cases[0], // 전남 총 확진자수
+          jeonnam_yesterday_confirmed_person: overall[15].cases[1], // 전남 어제 확진자수
+          
+          jeju_total_confirmed_person: overall[16].cases[0], // 제주 총 확진자수
+          jeju_yesterday_confirmed_person: overall[16].cases[1], // 제주 어제 확진자수
+      }
+  
+      let embed = new Discord.MessageEmbed()
+      embed.setTitle('코로나')
+      embed.setURL('https://corona-live.com')
+      embed.setColor('#FF8000')
+      embed.setDescription('증상이 있으실 경우 주변 접촉자에게 알리신 후 인근 보건소를 찾아주시기 바랍니다')
+      embed.addField(`대한민국 총 확진자수`, `${overview.total_confirmed_person}명`, true)
+      embed.addField(`어제 확진자수`, overview.yesterday_confirmed_person + `명`, true)
+      embed.addField(`오늘 확진자수(집계중)`, overview.current_confirmed_person + `명`, true)
+      embed.addField(`오늘 어제지금시간   -   현재지금시간의 확진자`, overview.current_confirmed_person_diff + `명`, true)
+      embed.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed.addField(`서울`, 'ㅤ')
+      embed.addField(`서울 총 확진자수`,  overall.seoul_total_confirmed_person + `명`, true)
+      embed.addField(`서울 어제 확진자수`, overall.seoul_yesterday_confirmed_person + `명`, true)
+      embed.addField(`서울 오늘 확진자 수(집계중)`, current.seoul_confirmed_person + `명`, true)
+      embed.addField(`서울 어제지금시간   -   현재지금시간의 확진자`, current.seoul_confirmed_person_diff,true)
+      embed.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed.addField(`부산` ,'ㅤ')
+      embed.addField(`부산 총 확진자수`,  overall.busan_total_confirmed_person + `명`, true)
+      embed.addField(`부산 어제 확진자수`, overall.busan_yesterday_confirmed_person + `명`, true)
+      embed.addField(`부산 현재 확진자 수(집계중)`, current.busan_confirmed_person + `명`, true)
+      embed.addField(`부산 어제지금시간   -   현재지금시간의 확진자`, current.busan_confirmed_person_diff + `명`,true)
+      embed.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed.addField(`대구` ,'ㅤ')
+      embed.addField(`대구 총 확진자수`,  overall.daegu_total_confirmed_person + `명`, true)
+      embed.addField(`대구 어제 확진자수`, overall.daegu_yesterday_confirmed_person + `명`, true)
+      embed.addField(`대구 현재 확진자 수(집계중)`, current.daegu_confirmed_person + `명`, true)
+      embed.addField(`대구 어제지금시간   -   현재지금시간의 확진자`, current.daegu_confirmed_person_diff + `명`,true)
+      embed.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      let embed2 = new Discord.MessageEmbed()
+      embed2.setColor('#FF8000')
+      embed2.addField(`광주` ,'ㅤ')
+      embed2.addField(`광주 총 확진자수`,  overall.gwangju_total_confirmed_person + `명`, true)
+      embed2.addField(`광주 어제 확진자수`, overall.gwangju_yesterday_confirmed_person + `명`, true)
+      embed2.addField(`광주 현재 확진자 수(집계중)`, current.gwangju_confirmed_person + `명`, true)
+      embed2.addField(`광주 어제지금시간   -   현재지금시간의 확진자`, current.gwangju_confirmed_person_diff + `명`,true)
+      embed2.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed2.addField(`대전` ,'ㅤ')
+      embed2.addField(`대전 총 확진자수`,  overall.daejeon_total_confirmed_person + `명`, true)
+      embed2.addField(`대전 어제 확진자수`, overall.daejeon_yesterday_confirmed_person + `명`, true)
+      embed2.addField(`대전 현재 확진자 수(집계중)`, current.daejeon_confirmed_person + `명`, true)
+      embed2.addField(`대전 어제지금시간   -   현재지금시간의 확진자`, current.daejeon_confirmed_person_diff + `명`,true)
+      embed2.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed2.addField(`울산` ,'ㅤ')
+      embed2.addField(`울산 총 확진자수`,  overall.ulsan_total_confirmed_person + `명`, true)
+      embed2.addField(`울산 어제 확진자수`, overall.ulsan_yesterday_confirmed_person + `명`, true)
+      embed2.addField(`울산 현재 확진자 수(집계중)`, current.ulsan_confirmed_person + `명`, true)
+      embed2.addField(`울산 어제지금시간   -   현재지금시간의 확진자`, current.ulsan_confirmed_person_diff + `명`,true)
+      embed2.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed2.addField(`세종` ,'ㅤ')
+      embed2.addField(`세종 총 확진자수`,  overall.sejong_total_confirmed_person + `명`, true)
+      embed2.addField(`세종 어제 확진자수`, overall.sejong_yesterday_confirmed_person + `명`, true)
+      embed2.addField(`세종 현재 확진자 수(집계중)`, current.sejong_confirmed_person + `명`, true)
+      embed2.addField(`세종 어제지금시간   -   현재지금시간의 확진자`, current.sejong_confirmed_person_diff + `명`,true)
+      embed2.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      let embed3 = new Discord.MessageEmbed()
+      embed3.setColor('#FF8000')
+      embed3.addField(`경기` ,'ㅤ')
+      embed3.addField(`경기 총 확진자수`,  overall.gyeonggi_total_confirmed_person + `명`, true)
+      embed3.addField(`경기 어제 확진자수`, overall.gyeonggi_yesterday_confirmed_person + `명`, true)
+      embed3.addField(`경기 현재 확진자 수(집계중)`, current.gyeonggi_confirmed_person + `명`, true)
+      embed3.addField(`경기 어제  지금시간   -   현재지금시간의 확진자`, current.gyeonggi_confirmed_person_diff + `명`,true)
+      embed3.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed3.addField(`강원` ,'ㅤ')
+      embed3.addField(`강원 총 확진자수`,  overall.gangwon_total_confirmed_person + `명`, true)
+      embed3.addField(`강원 어제 확진자수`, overall.gangwon_yesterday_confirmed_person + `명`, true)
+      embed3.addField(`강원 현재 확진자 수(집계중)`, current.gangwon_confirmed_person + `명`, true)
+      embed3.addField(`강원 어제  지금시간   -   현재지금시간의 확진자`, current.gangwon_confirmed_person_diff + `명`,true)
+      embed3.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed3.addField(`충북` ,'ㅤ')
+      embed3.addField(`충북 총 확진자수`,  overall.chungbuk_total_confirmed_person + `명`, true)
+      embed3.addField(`충북 어제 확진자수`, overall.chungbuk_yesterday_confirmed_person + `명`, true)
+      embed3.addField(`충북 현재 확진자 수(집계중)`, current.chungbuk_confirmed_person + `명`, true)
+      embed3.addField(`충북 어제  지금시간   -   현재지금시간의 확진자`, current.chungbuk_confirmed_person_diff + `명`,true)
+      embed3.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed3.addField(`충남` ,'ㅤ')
+      embed3.addField(`충남 총 확진자수`,  overall.chungnam_total_confirmed_person + `명`, true)
+      embed3.addField(`충남 어제 확진자수`, overall.chungnam_yesterday_confirmed_person + `명`, true)
+      embed3.addField(`충남 현재 확진자 수(집계중)`, current.chungnam_confirmed_person + `명`, true)
+      embed3.addField(`충남 어제  지금시간   -   현재지금시간의 확진자`, current.chungnam_confirmed_person_diff + `명`,true)
+      embed3.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      let embed4 = new Discord.MessageEmbed()
+      embed4.setColor('#FF8000')
+      embed4.addField(`경북` ,'ㅤ')
+      embed4.addField(`경북 총 확진자수`,  overall.gyeongbuk_total_confirmed_person + `명`, true)
+      embed4.addField(`경북 어제 확진자수`, overall.gyeongbuk_yesterday_confirmed_person + `명`, true)
+      embed4.addField(`경북 현재 확진자 수(집계중)`, current.gyeongbuk_confirmed_person + `명`, true)
+      embed4.addField(`경북 어제  지금시간   -   현재지금시간의 확진자`, current.gyeongbuk_confirmed_person_diff + `명`,true)
+      embed4.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed4.addField(`경남` ,'ㅤ')
+      embed4.addField(`경남 총 확진자수`,  overall.gyeongnam_total_confirmed_person + `명`, true)
+      embed4.addField(`경남 어제 확진자수`, overall.gyeongnam_yesterday_confirmed_person + `명`, true)
+      embed4.addField(`경남 현재 확진자 수(집계중)`, current.gyeongnam_confirmed_person + `명`, true)
+      embed4.addField(`경남 어제  지금시간   -   현재지금시간의 확진자`, current.gyeongnam_confirmed_person_diff + `명`,true)
+      embed4.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed4.addField(`전북` ,'ㅤ')
+      embed4.addField(`전북 총 확진자수`,  overall.jeonbuk_total_confirmed_person + `명`, true)
+      embed4.addField(`전북 어제 확진자수`, overall.jeonbuk_yesterday_confirmed_person + `명`, true)
+      embed4.addField(`전북 현재 확진자 수(집계중)`, current.jeonbuk_confirmed_person + `명`, true)
+      embed4.addField(`전북 어제  지금시간   -   현재지금시간의 확진자`, current.jeonbuk_confirmed_person_diff + `명`,true)
+      embed4.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      embed4.addField(`전남` ,'ㅤ')
+      embed4.addField(`전남 총 확진자수`,  overall.jeonnam_total_confirmed_person + `명`, true)
+      embed4.addField(`전남 어제 확진자수`, overall.jeonnam_yesterday_confirmed_person + `명`, true)
+      embed4.addField(`전남 현재 확진자 수(집계중)`, current.jeonnam_confirmed_person + `명`, true)
+      embed4.addField(`전남 어제  지금시간   -   현재지금시간의 확진자`, current.jeonnam_confirmed_person_diff + `명`,true)
+      embed4.addField(`--------------------------------------------------------------------------------------------------`, 'ㅤ')
+      let embed5 = new Discord.MessageEmbed()
+      embed5.setColor(`#FF8000`)
+      embed5.addField(`제주` ,'ㅤ')
+      embed5.addField(`제주 총 확진자수`,  overall.jeju_total_confirmed_person + `명`, true)
+      embed5.addField(`제주 어제 확진자수`, overall.jeju_yesterday_confirmed_person + `명`, true)
+      embed5.addField(`제주 현재 확진자 수(집계중)`, current.jeju_confirmed_person + `명`, true)
+      embed5.addField(`제주 어제  지금시간   -   현재지금시간의 확진자`, current.jeju_confirmed_person_diff + `명`,true)
+      message.channel.send(embed)
+      message.channel.send(embed2)
+      message.channel.send(embed3)
+      message.channel.send(embed4)
+      message.channel.send(embed5)
+  })
+}
+ 
+
 
 });
 
